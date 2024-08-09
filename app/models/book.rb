baada9f0c5a573b9fc5f1bfb86655e4708fdaa91
gugugngn <173164252+gugugngn@users.noreply.github.com>
2024-08-09 07:47:46 +0000

    [Finish]検索機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,4 +10,15 @@ class Book < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
   
+    def self.search_for(content,method)
+      if method == 'perfect'
+        User.where(name: content)
+      elsif method == 'forward'
+        User.where('name LIKE ?', content + '%')
+      elsif method == 'backward'
+        User.where('name LIKE ?', '%'  + content)
+      else
+        User.where('name LIKE ?', '%' +  content  + '%')
+      end 
+    end
 end
